9c2713053ecf2b865c6952f42749154c390f9765
pca006132 <john.lck40@gmail.com>
2021-04-01 18:41:03 +0800

    clean appropriately

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,12 @@
-CLEAN="mkdir -p bin && mkdir -p target/test-classes"
 CLASSES="comp5111.assignment.cut.ToolBox comp5111.assignment.cut.ToolBox\\\$ArrayTools comp5111.assignment.cut.ToolBox\\\$CharSequenceTools comp5111.assignment.cut.ToolBox\\\$CharTools comp5111.assignment.cut.ToolBox\\\$LocaleTools comp5111.assignment.cut.ToolBox\\\$RegExTools comp5111.assignment.cut.ToolBox\\\$StringTools"
 
 BUILD_CMD="javac -cp \"./lib/junit-4.12.jar:./lib/evosuite-standalone-runtime-1.1.0.jar:bin\" -d ./target/test-classes/"
 RUN_CMD="java -classpath \"./target/classes:./lib/evosuite-standalone-runtime-1.1.0.jar:./lib/junit-4.12.jar:./lib/hamcrest-core-1.3.jar:./lib/soot-4.2.1-jar-with-dependencies.jar\" comp5111.assignment.Assignment2"
 
 function get_report() {
-    eval $CLEAN
+    rm -r target/test-classes
+    rm -r sootOutput
+    mkdir -p target/test-classes
     eval $BUILD_CMD $1/comp5111/assignment/cut/$3.java $1/comp5111/assignment/cut/$4.java
     eval $RUN_CMD $1/spectrum_fl_jaccard_$2.csv Jaccard comp5111.assignment.cut.$3 $CLASSES
     eval $RUN_CMD $1/spectrum_fl_tarantula_$2.csv Tarantula comp5111.assignment.cut.$3 $CLASSES
