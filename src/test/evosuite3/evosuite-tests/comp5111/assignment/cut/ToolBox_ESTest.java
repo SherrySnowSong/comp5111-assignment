dca682e82d2724c4ef05f4473590778b498efff8
pca006132 <john.lck40@gmail.com>
2021-04-02 20:59:46 +0800

    task 1 tested

--- src/test/evosuite3/evosuite-tests/comp5111/assignment/cut/ToolBox_ESTest.java
+++ src/test/evosuite3/evosuite-tests/comp5111/assignment/cut/ToolBox_ESTest.java
@@ -19,7 +19,6 @@ import org.evosuite.runtime.EvoRunnerParameters;
 import org.evosuite.runtime.ViolatedAssumptionAnswer;
 import org.junit.runner.RunWith;
 
-@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true, useJEE = true) 
 public class ToolBox_ESTest extends ToolBox_ESTest_scaffolding {
 
   @Test(timeout = 4000)
